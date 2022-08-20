eb72af0c6fadf6c8df6ef2f9ad3be2e86e426b95
dkbs <45911671+deutwaffe@users.noreply.github.com>
2022-08-21 07:27:57 +0800

    format

--- main.js
+++ main.js
@@ -3,45 +3,45 @@ import "@tensorflow/tfjs-backend-wasm";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 
 function status(text) {
-  document.getElementById("status").textContent = text;
+    document.getElementById("status").textContent = text;
 }
 
 function result(predictions) {
-  let resultElement = document.getElementById("result");
-  resultElement.innerHTML = "";
-  predictions.forEach((element) => {
-    let class_name = element.className;
-    let probability = element.probability;
-    let text = class_name + probability + `\n`;
-    resultElement.innerHTML +=
-      "<li>" + class_name + "\t" + probability + "</li>";
-  });
+    let resultElement = document.getElementById("result");
+    resultElement.innerHTML = "";
+    predictions.forEach((element) => {
+        let class_name = element.className;
+        let probability = element.probability;
+        let text = class_name + probability + `\n`;
+        resultElement.innerHTML +=
+            "<li>" + class_name + "\t" + probability + "</li>";
+    });
 }
 
 async function main() {
-  await tf.setBackend("wasm");
-  const model = await mobilenet.load();
+    await tf.setBackend("wasm");
+    const model = await mobilenet.load();
 
-  const startTime1 = performance.now();
-  const imgElement = document.getElementById("img");
-  status("Model loaded!");
+    const startTime1 = performance.now();
+    const imgElement = document.getElementById("img");
+    status("Model loaded!");
 
-  let img = tf.browser
-    .fromPixels(imgElement)
-    .resizeBilinear([224, 224])
-    .expandDims(0)
-    .toFloat();
+    let img = tf.browser
+        .fromPixels(imgElement)
+        .resizeBilinear([224, 224])
+        .expandDims(0)
+        .toFloat();
 
-  let startTime2 = performance.now();
-  const predictions = await model.classify(img);
+    let startTime2 = performance.now();
+    const predictions = await model.classify(img);
 
-  const totalTime1 = performance.now() - startTime1;
-  const totalTime2 = performance.now() - startTime2;
-  status(
-    `Done in ${Math.floor(totalTime1)} ms ` +
-      `(not including preprocessing: ${Math.floor(totalTime2)} ms)`
-  );
-  result(predictions);
+    const totalTime1 = performance.now() - startTime1;
+    const totalTime2 = performance.now() - startTime2;
+    status(
+        `Done in ${Math.floor(totalTime1)} ms ` +
+            `(not including preprocessing: ${Math.floor(totalTime2)} ms)`
+    );
+    result(predictions);
 }
 
 document.addEventListener("DOMContentLoaded", main);
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default {
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "node_modules/@tensorflow/tfjs-backend-wasm/dist/*.wasm",
-          dest: "/",
-        },
-      ],
-    }),
-  ],
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "node_modules/@tensorflow/tfjs-backend-wasm/dist/*.wasm",
+                    dest: "/",
+                },
+            ],
+        }),
+    ],
 };
